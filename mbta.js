fbf8b2b66bc6a94b08cdd850a2dc5ec4d352e2db
tsyan <anna.tsykalova@gmail.com>
2014-02-18 16:53:30 -0500

    defined green, red, and orange lines as arrays

--- mbta.js
+++ mbta.js
@@ -9,13 +9,13 @@ startingStation = prompt("Enter the Starting Station");
 endingLine = prompt("Enter the Ending Line");
 endingStation =  prompt("Enter the Ending Station");
 
-alert("Staring at " + startingLine + " : " + startingStation);
+alert("Starting at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var lines = {
+  "green":["haymarket", "government center", "park st", "boylston", "arlington", "copley"],
+  "red": ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  "orange": ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
 };
 
