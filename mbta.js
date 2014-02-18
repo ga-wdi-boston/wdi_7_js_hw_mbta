21c06052bf79f43190dd4c487a870214c95c4da3
Laure Fischer <laurefischer27@gmail.com>
2014-02-18 16:51:52 -0500

    initial commit

--- mbta.js
+++ mbta.js
@@ -9,13 +9,13 @@ startingStation = prompt("Enter the Starting Station");
 endingLine = prompt("Enter the Ending Line");
 endingStation =  prompt("Enter the Ending Station");
 
-alert("Staring at " + startingLine + " : " + startingStation);
+alert("Starting at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var lines = {
+  'green':['Haymarket Green', 'Government Center', 'Park St', 'Boylston', 'Arlington', 'Copley'],
+  'red': ['South Station', 'Park St', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+  'orange': ['Forest Hills', 'Back Bay', 'Chinatown', 'Downtown Crossing', 'State', 'Park St', 'Haymarket Orange', 'North Station']
 };
 
