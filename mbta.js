f5554e98e35b4c04d1bd43d10e156677dda1fe9a
Jordan Morano <jordan.morano@gmail.com>
2014-02-18 17:31:26 -0500

    Create mbta object with user prompts and calc stop method

--- mbta.js
+++ mbta.js
@@ -1,21 +1,28 @@
 // Code here.
-var startingLine,
-startingStation,
-endingLine,
-endingStation;
-
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
-
-alert("Staring at " + startingLine + " : " + startingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
-
-// Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var mbta = {
+  start_line: prompt("Enter the Starting Line: "),
+  start_stop: prompt("Enter the Starting Station"),
+  end_line: prompt("Enter the Ending Line"),
+  end_stop: prompt("Enter the Ending Station"),
+  lines: {
+    'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
+    'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
+    'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+  },
+  calc_stops: function (start_line, start_stop, end_line, end_stop) {
+    var to_park, from_park;
+    if ( start_line === end_line ) {
+      return Math.abs( this.lines[start_line].indexOf( start_stop ) - this.lines[end_line].indexOf( end_stop ) );
+    } else {
+      to_park = Math.abs( this.lines[start_line].indexOf( start_stop ) - this.lines[start_line].indexOf( "park st" ) );
+      from_park = Math.abs( this.lines[end_line].indexOf( "park st" ) - this.lines[end_line].indexOf( end_stop ) );
+      return to_park + from_park;
+    }
+  }
 };
 
+alert("Staring at " + mbta.start_line + " : " + mbta.start_stop);
+alert("Ending at " + mbta.end_line + " : " + mbta.end_stop);
+
+result = mbta.calc_stops(mbta.start_line.toLowerCase(), mbta.start_stop.toLowerCase(), mbta.end_line.toLowerCase(), mbta.end_stop.toLowerCase());
+console.log("It will take you " + result + " stops for this trip.");
