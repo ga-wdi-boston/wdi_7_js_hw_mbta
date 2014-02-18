0e73d897a402fd068eec2ec28e83a8a8ecc0582a
Jordan Morano <jordan.morano@gmail.com>
2014-02-18 17:38:16 -0500

    Refactor redundant code using keyword this

--- mbta.js
+++ mbta.js
@@ -1,21 +1,21 @@
-// Code here.
+// MBTA object
 var mbta = {
-  start_line: prompt("Enter the Starting Line: "),
-  start_stop: prompt("Enter the Starting Station"),
-  end_line: prompt("Enter the Ending Line"),
-  end_stop: prompt("Enter the Ending Station"),
+  start_line: prompt("Enter the Starting Line: ").toLowerCase(),
+  start_stop: prompt("Enter the Starting Station").toLowerCase(),
+  end_line: prompt("Enter the Ending Line").toLowerCase(),
+  end_stop: prompt("Enter the Ending Station").toLowerCase(),
   lines: {
     'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
     'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
     'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
   },
-  calc_stops: function (start_line, start_stop, end_line, end_stop) {
+  calc_stops: function () {
     var to_park, from_park;
-    if ( start_line === end_line ) {
-      return Math.abs( this.lines[start_line].indexOf( start_stop ) - this.lines[end_line].indexOf( end_stop ) );
+    if ( this.start_line === this.end_line ) {
+      return Math.abs( this.lines[this.start_line].indexOf( this.start_stop ) - this.lines[this.end_line].indexOf( this.end_stop ) );
     } else {
-      to_park = Math.abs( this.lines[start_line].indexOf( start_stop ) - this.lines[start_line].indexOf( "park st" ) );
-      from_park = Math.abs( this.lines[end_line].indexOf( "park st" ) - this.lines[end_line].indexOf( end_stop ) );
+      to_park = Math.abs( this.lines[this.start_line].indexOf( this.start_stop ) - this.lines[this.start_line].indexOf( "park st" ) );
+      from_park = Math.abs( this.lines[this.end_line].indexOf( "park st" ) - this.lines[this.end_line].indexOf( this.end_stop ) );
       return to_park + from_park;
     }
   }
@@ -24,5 +24,5 @@ var mbta = {
 alert("Staring at " + mbta.start_line + " : " + mbta.start_stop);
 alert("Ending at " + mbta.end_line + " : " + mbta.end_stop);
 
-var result = mbta.calc_stops(mbta.start_line.toLowerCase(), mbta.start_stop.toLowerCase(), mbta.end_line.toLowerCase(), mbta.end_stop.toLowerCase());
+var result = mbta.calc_stops();
 console.log("It will take you " + result + " stops for this trip.");
