029c66dd4f607b0c83718660a4c7446b04d7775e
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-18 18:03:44 -0500

    function realized

--- mbta.js
+++ mbta.js
@@ -13,9 +13,22 @@ alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var lines = {
+  'green':['green/haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
+  'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
+  'orange': ['north station', 'orange/haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
 };
+
+var station_count = function(startingLine, startingStation, endingLine, endingStation){
+  var count = 0;
+  if (startingLine === endingLine){
+    count = Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf(endingStation));
+  } else {
+    count = (Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf('park st')) + Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf('park st')));
+  };
+  return count;
+}
+var stops = station_count(startingLine, startingStation, endingLine, endingStation);
+alert('You will take ' + stops + ' stations totally!');
+
 
