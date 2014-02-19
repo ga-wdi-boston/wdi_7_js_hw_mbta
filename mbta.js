3ea473f7d73672d13f2647f3a9c008c5a072ab6b
K Mak <karen.mak@stx.oxon.org>
2014-02-18 21:39:46 -0500

    can calculate stops

--- mbta.js
+++ mbta.js
@@ -2,7 +2,11 @@
 var startingLine,
 startingStation,
 endingLine,
-endingStation;
+endingStation,
+distanceToIntersection,
+index,
+startToIntersection,
+totalDistance;
 
 startingLine = prompt("Enter the Starting Line: ");
 startingStation = prompt("Enter the Starting Station");
@@ -14,8 +18,28 @@ alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
 lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+  'green':['Haymarket', 'Government center', 'Park Street', 'Bolyston', 'Arlington', 'Copley'],
+  'red': ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+  'orange': ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back bay', 'Forest Hills']
 };
 
+// not much javascript
+// startDistance = Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf('Park Street'));
+// endDistance = Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf('Park Street'));
+// totalDistance = startDistance + endDistance;
+
+// some more javascript
+
+
+var distanceToIntersection = function(line, station) {
+    var index = Math.abs(lines[line].indexOf(station) - lines[line].indexOf('Park Street'));
+    return index;
+  }
+
+
+startToIntersection = distanceToIntersection(startingLine, startingStation);
+endToIntersection = distanceToIntersection(endingLine, endingStation);
+
+totalDistance = (startToIntersection + endToIntersection);
+
+alert("The total number of stops is " + totalDistance + " stops.");
