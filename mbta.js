c774f597d24b371f28dfe963f236c7b29531fe41
Mike Stone <mikestone14@gmail.com>
2014-02-19 12:44:40 -0500

    Further refactored namespacing. All functions in namespaced object

--- mbta.js
+++ mbta.js
@@ -11,20 +11,24 @@ var MBTA = {
 					var current_stop = current_line.indexOf(stop);
 					return Math.abs(current_stop - park);
 				}
+			},
+			prompts: function() {
+				this.startingLine = prompt("Enter the Starting Line: ");
+				this.startingStation = prompt("Enter the Starting Station");
+				this.endingLine = prompt("Enter the Ending Line");
+				this.endingStation =  prompt("Enter the Ending Station");
+			},
+			calc_distances: function() {
+				this.start_parkst = this.distance_to_park(this.startingLine);
+				this.end_parkst = this.distance_to_park(this.endingLine);
+				this.first_stop = this.start_parkst(this.startingStation);
+				this.ending_stop = this.end_parkst(this.endingStation);
+				return this.first_stop + this.ending_stop;
 			}
 	};
 
-MBTA.startingLine = prompt("Enter the Starting Line: ");
-MBTA.startingStation = prompt("Enter the Starting Station");
-MBTA.endingLine = prompt("Enter the Ending Line");
-MBTA.endingStation =  prompt("Enter the Ending Station");
+// These variables are dynamically defined.
+MBTA.prompts();
 
-MBTA.start_parkst = MBTA.distance_to_park(MBTA.startingLine);
-MBTA.end_parkst = MBTA.distance_to_park(MBTA.endingLine);
-MBTA.first_stop = MBTA.start_parkst(MBTA.startingStation);
-MBTA.ending_stop = MBTA.end_parkst(MBTA.endingStation);
-
-MBTA.total_distance = MBTA.first_stop + MBTA.ending_stop;
-
-alert("The total number of stops for your travel is " + MBTA.total_distance + " stops.");
+alert("The total number of stops for your travel is " + MBTA.calc_distances() + " stops.");
 
