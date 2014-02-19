f8bce37ca895752f9db184ecf5dc418d65b486ff
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-02-18 23:27:14 -0500

    nothing solved - working things out

--- mbta.js
+++ mbta.js
@@ -1,8 +1,14 @@
-// Code here.
-var startingLine,
+var lines,
+startingLine,
 startingStation,
 endingLine,
 endingStation;
+
+lines = {
+  'green': ["haymarket", "government center", "park street", "bolyston", "arlington", "copley"],
+  'red': ["south station", "park street", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  'orange': ["north station", "haymarket", "park street", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+};
 
 startingLine = prompt("Enter the Starting Line: ");
 startingStation = prompt("Enter the Starting Station");
@@ -12,10 +18,22 @@ endingStation =  prompt("Enter the Ending Station");
 alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
-// Create a array for each line
-var lines = {
-  'green': ["haymarket", "government center", "park street", "bolyston", "arlington", "copley"],
-  'red': ["south station", "park street", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-  'orange': ["north station", "haymarket", "park street", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+var withoutSwitch = function(line, startingStation, endingStation) {
+  var startingStation_index = lines[line].indexOf(startingStation);
+  var endingStation_index = lines[line].indexOf(endingStation);
+
+  return Math.abs(startingStation_index - endingStation_index);
 };
 
+var withSwitch = function(line, startingStation, endingStation) {
+  var startingStation_index = lines[line].indexOf(startingStation);
+  var intersection_index = lines[line].indexOf("park street");
+  var endingStation_index = lines[line].indexOf(endingStation);
+
+  return
+};
+
+// do something with this maybe? or not necessary?
+// if(startingLine === endingLine) {
+
+// }
