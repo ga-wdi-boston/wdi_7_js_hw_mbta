22cccd0756b2a9355211c79605ebd3394f481dfa
Matt Higgins <higginsmt@gmail.com>
2014-02-19 09:08:53 -0500

    JS mbta

--- mbta.js
+++ mbta.js
@@ -12,10 +12,37 @@ endingStation =  prompt("Enter the Ending Station");
 alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
-// Create a array for each line
+// Create an array for each line
 lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+  'red': [
+		"south station",	 		// :red[0]
+		"park st",				 		// :red[1] *
+		"kendall",					  // :red[2]
+		"central",					  // :red[3]
+		"harvard",						// :red[4]
+		"porter",							// :red[5]
+		"davis",							// :red[6]
+		"alewife"							// :red[7]
+	],
+	'green': [
+		"haymarket",					// :green[0]
+		"government center",	// :green[1]
+		"park st",						// :green[2] *
+		"boylston",						// :green[3]
+		"arlington",					// :green[4]
+		"copley"							// :green[5]
+	],
+	'orange': [
+		"north station",			// :orange[0]
+		"haymarket",					// :orange[1]
+		"park st",						// :orange[2] *
+		"state",							// :orange[3]
+		"downtown crossing",	// :orange[4]
+		"chinatown",					// :orange[5]
+		"back bay",						// :orange[6]
+		"forest hills"				// :orange[7]
+	]
 };
+
+
 
