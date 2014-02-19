227630bc0470d207b2b66844041bcfd43a25868e
Yiyang Song <songyiyang2012@gmail.com>
2014-02-19 01:20:29 -0500

    use inclass function and make it perfect

--- mbta.js
+++ mbta.js
@@ -1,26 +1,24 @@
-
-var startingLine = prompt("Enter the Starting Line: "),
-		startingStation = prompt("Enter the Starting Station"),
-		endingLine = prompt("Enter the Ending Line"),
-		endingStation =  prompt("Enter the Ending Station"),
-		lines = {'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
-  					 'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-  					 'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
-  					};
-
-var tripCount = function(startingLine, startingStation, endingLine, endingStation){
-	if(lines.startingLine === lines.endingLine){
-		return Math.abs(lines[startingLine].indexOf(startingStation) - lines[endingLine].indexOf(endingStation));
+var tripCount = function(){
+	var lines = {'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
+  					   'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  					   'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+  					  };
+  return function(){
+  	var startingLine = prompt("Enter the Starting Line: "),
+				startingStation = prompt("Enter the Starting Station"),
+				endingLine = prompt("Enter the Ending Line"),
+				endingStation =  prompt("Enter the Ending Station");
+		alert("Staring at " + startingLine + " : " + startingStation);
+		alert("Ending at " + endingLine + " : " + endingStation);
+  	if(startingLine === endingLine){
+			return Math.abs(lines[startingLine].indexOf(startingStation) - lines[endingLine].indexOf(endingStation));
+		} else {
+			return (Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf("part st")) + Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf("park st")));
+		}
 	}
-	else {
-		return (Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf("part st")) + Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf("part st")))
-	}
-}
+}, stopCount = tripCount();
 
-alert("Staring at " + startingLine + " : " + startingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
-alert("Total number of stops for the trips is " + tripCount(startingLine, startingStation, endingLine, endingStation));
-
+alert("Total number of stops for the trips is " + stopCount());
 
 
 
