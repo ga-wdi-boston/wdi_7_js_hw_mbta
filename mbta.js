70daec997ea0a9195cf391c735263f5e0d49ceb9
Mike Stone <mikestone14@gmail.com>
2014-02-19 12:37:15 -0500

    Refactored namespaced mbta lab

--- mbta.js
+++ mbta.js
@@ -1,39 +1,30 @@
-var startingLine,
-	startingStation,
-	endingLine,
-	endingStation,
-	lines,
-	total_distance,
-	start_parkst,
-	end_parkst,
-	first_stop,
-	ending_stop,
-	mbta = {
-		lines: {
-			'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
-			'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
-			'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
-		},
-		distance_to_park: function(line) {
-			var current_line = mbta.lines[line], park = current_line.indexOf('park st');
-			return function(stop) {
-				var current_stop = current_line.indexOf(stop);
-				return Math.abs(current_stop - park);
+var MBTA = {
+			lines: {
+				'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
+				'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
+				'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+			},
+			distance_to_park: function(line) {
+				var current_line = this.lines[line],
+						park = current_line.indexOf('park st');
+				return function(stop) {
+					var current_stop = current_line.indexOf(stop);
+					return Math.abs(current_stop - park);
+				}
 			}
-		}
 	};
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+MBTA.startingLine = prompt("Enter the Starting Line: ");
+MBTA.startingStation = prompt("Enter the Starting Station");
+MBTA.endingLine = prompt("Enter the Ending Line");
+MBTA.endingStation =  prompt("Enter the Ending Station");
 
-start_parkst = mbta.distance_to_park(startingLine);
-end_parkst = mbta.distance_to_park(endingLine);
-first_stop = start_parkst(startingStation);
-ending_stop = end_parkst(endingStation);
+MBTA.start_parkst = MBTA.distance_to_park(MBTA.startingLine);
+MBTA.end_parkst = MBTA.distance_to_park(MBTA.endingLine);
+MBTA.first_stop = MBTA.start_parkst(MBTA.startingStation);
+MBTA.ending_stop = MBTA.end_parkst(MBTA.endingStation);
 
-total_distance = first_stop + ending_stop;
+MBTA.total_distance = MBTA.first_stop + MBTA.ending_stop;
 
-alert("The total number of stops for your travel is " + total_distance + " stops.");
+alert("The total number of stops for your travel is " + MBTA.total_distance + " stops.");
 
