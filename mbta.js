00cd8e0728eed3e8e533b9669b4fe1a14782f1bd
Yiyang Song <songyiyang2012@gmail.com>
2014-02-19 19:02:52 -0500

    created html input

--- mbta.js
+++ mbta.js
@@ -1,24 +1,24 @@
-var tripCount = function(){
-	var lines = {'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
-  					   'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
-  					   'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
-  					  };
-  return function(){
-  	var startingLine = prompt("Enter the Starting Line: "),
-				startingStation = prompt("Enter the Starting Station"),
-				endingLine = prompt("Enter the Ending Line"),
-				endingStation =  prompt("Enter the Ending Station");
-		alert("Staring at " + startingLine + " : " + startingStation);
-		alert("Ending at " + endingLine + " : " + endingStation);
+var MbtaApp = {
+	lines: {'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
+	  			 'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+	  			 'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+	  			},
+  tripCount : function(form){
+  	var startingLine = form.StartingLine.value,
+  		  startingStation = form.StartingStation.value,
+  		  endingLine = form.EndingLine.value,
+  		  endingStation = form.EndingStation.value,
+  		  countStop;
   	if(startingLine === endingLine){
-			return Math.abs(lines[startingLine].indexOf(startingStation) - lines[endingLine].indexOf(endingStation));
+			countStop = (Math.abs(this.lines[startingLine].indexOf(startingStation) - this.lines[endingLine].indexOf(endingStation)));
 		} else {
-			return (Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf("part st")) + Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf("park st")));
+			countStop = ((Math.abs(this.lines[startingLine].indexOf(startingStation) - this.lines[startingLine].indexOf("park st")) + Math.abs(this.lines[endingLine].indexOf(endingStation) - this.lines[endingLine].indexOf("park st"))));
 		}
+		alert("Total number of stops for the trips is " + countStop);
 	}
-}, stopCount = tripCount();
+};
 
-alert("Total number of stops for the trips is " + stopCount());
+
 
 
 
