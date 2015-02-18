d988118fb4695be0536eeabc1c7dd5d3af02ebb1
idelairre <idelairre@gmail.com>
2015-02-17 20:36:34 -0500

    organized given variables for prompts into a function. Lightly referencing Tom, Dave's and Caitlyn's ruby solition. Trying not to spoil it too much but I'm feeling pressured to move on to the other assignment.

--- mbta.js
+++ mbta.js
@@ -1,21 +1,34 @@
-// Code here.
-var startingLine,
-startingStation,
-endingLine,
-endingStation;
+var MBTA = {
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+lines = {
+  "green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
+  "red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
+  "orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
+  },
+
+  userInputs = function(){
+    startingLine = prompt("Enter the Starting Line: ");
+    endingLine = prompt("Enter the Ending Line");
+    startingStation = prompt("Enter the Starting Station");
+    endingStation =  prompt("Enter the Ending Station");
+  },
+
+  // distance = mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)
+  // distance = distance.abs
+
+  distanceToParkSt = function(line){
+      var startingLine = this.lines[line],
+      parkSt = startingLine.indexOf("Park Street");
+      return function(endingStation){
+
+  }
+
+  startingStation,
+  endingLine,
+  endingStation;
+
 
 alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
-// Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
 };
-
