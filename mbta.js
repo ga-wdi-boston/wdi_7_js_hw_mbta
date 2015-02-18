52436e900eaf5df50815ead2e3ff596e7649d542
brandonkoo <brandonkoo555@gmail.com>
2015-02-18 09:07:40 -0500

    created some mbta test to check for var and array

--- mbta.js
+++ mbta.js
@@ -4,18 +4,50 @@ startingStation,
 endingLine,
 endingStation;
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+// startingLine = prompt("Enter the Starting Line: ");
+// startingStation = prompt("Enter the Starting Station");
+// endingLine = prompt("Enter the Ending Line");
+// endingStation =  prompt("Enter the Ending Station");
+
+
+
 
 alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
+
+
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var mbta = {
+  green:
+    ["haymarket", "government center", "park st", "bolyston", "arlington", "copley"];
+
+  red:
+    ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"];
+
+  orange:
+    ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"];
+
 };
+
+var numberStops = function(startingLine, startingStation, stopline, stopstation){
+  if (startingLine === stopline){
+    return MBTA.abs(MBTA[startingLine].indexof(startingStation) - MBTA[stopline].indexof(stopstation));
+} else {
+  return MBTA.abs(MBTA[startingLine].indexof(startingStation) - MBTA[stopline].indexof('park st')+ MBTA.abs(MBTA[stopLine].indexof('park st') - MBTA[stopline].indexof(stopstation));
+
+}
+
+};
+
+
+
+// are tge stops on the same line?
+// take the absolute value of diff of stop indexex on the same line
+
+//diff between start/park, stop/park
+
+
+
+
 
---
+++ mbta_spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+describe('MBTA in javascript',function(){
+  it('first of all, it is a function',function(){
+    expect(MBTA).toEqual(jasmine.any(Function));
+    expect(MBTA).toBeDefined();
+  });
+
+
+  describe('green in javascript',Array(){
+  it('green, it is a array contains train stop',Array(){
+    expect(green).toContain(jasmine.any(Array));
+    expect(MBTA).toBeDefined();
+  });
+
+  describe('red in javascript',Array(){
+  it('red, it is a array contains train stop',Array(){
+    expect(red).toContain(jasmine.any(Array));
+    expect(red).toBeDefined();
+  });
+
+  describe('orange in javascript',Array(){
+  it('orange, it is a array contains train stop',Array(){
+    expect(orange).toContain(jasmine.any(Array));
+    expect(orange).toBeDefined();
+  });
+
+  describe('startingLine in javascript',var(){
+  it('startingLine, it is a array contains train stop',var(){
+    expect(startingLine).toEqual(jasmine.any(var));
+    expect(startingLine).toBeDefined();
+  });
+
+  describe('startingStation in javascript',var(){
+  it('startingStation, it is a array contains train stop',var(){
+    expect(startingStation).toEqual(jasmine.any(var));
+    expect(startingStation).toBeDefined();
+  });
+
+  describe('endingLine in javascript',var(){
+  it('endingLine, it is a array contains train stop',var(){
+    expect(endingLine).toEqual(jasmine.any(var));
+    expect(endingLine).toBeDefined();
+  });
+
+
