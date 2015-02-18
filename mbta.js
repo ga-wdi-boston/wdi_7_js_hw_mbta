05c38df324aa08c8d163daf396573a20083bfce1
idelairre <idelairre@gmail.com>
2015-02-17 21:13:45 -0500

    there seems to be a problem with my adaptation of Dave's solution, I copied and pasted Dave's solution as well to see if it would run and the same 'cannot read property 'indexOf' undefined emerged.

--- mbta.js
+++ mbta.js
@@ -1,40 +1,40 @@
 var MBTA = {
 
-lines: {
-  "green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
-  "red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
-  "orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
+  lines: {
+    "green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
+    "red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
+    "orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
+    },
+
+    userInputs: function(){
+      this.startingLine = prompt("Enter the Starting Line: ");
+      this.startingStation = prompt("Enter the Starting Station");
+      this.endingLine = prompt("Enter the Ending Line");
+      this.endingStation =  prompt("Enter the Ending Station");
+    },
+
+    // distance = mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)
+    // distance = distance.abs
+
+  distanceToPark: function(line) {
+    var currentLine = this.lines[line],
+    parkSt = currentLine.indexOf("park street");
+    return function(station) {
+      var currentStop = currentLine.indexOf(station);
+      return Math.abs(currentStop - parkSt);
+    }
   },
 
-  userInputs: function(){
-    this.startingLine = prompt("Enter the Starting Line: ");
-    this.endingLine = prompt("Enter the Ending Line");
-    this.startingStation = prompt("Enter the Starting Station");
-    this.endingStation =  prompt("Enter the Ending Station");
-  },
+    distanceToStation: function(){
+      this.startPark = this.distanceToPark(this.startingLine);
+      this.endPark = this.distanceToPark(this.endingLine);
+      this.firstStop = this.startPark(this.startingStation);
+      this.endingStop = this.endPark(this.endingStation);
+      return this.firstStop + this.endingStop;
+    }
+  };
 
-  // distance = mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)
-  // distance = distance.abs
-
-  distanceToParkSt: function(line){
-      var currentLine = this.lines[line],
-      parkSt = currentLine.indexOf("Park Street");
-      return function(endingStation){
-        var currentStation = currentLine.indexOf(endingStation);
-        return Math.abs(currentStation - ParkSt);
-      }
-  },
-
-  distanceToStation: function(){
-    this.startParkSt = this.distanceToParkSt(this.startingLine);
-    this.endParkSt = this.distanceToParkSt(this.endingLine);
-    this.firstStop = this.startParkSt(this.startingStation);
-    this.endingStop = this.endParkSt(this.endingStation);
-    return this.firstStop + this.endingStop;
-  }
-};
-
-MBTA.prompts();
+MBTA.userInputs();
 alert("The total number of stops for your travel is " + MBTA.distanceToStation() + " stops.");
 
 
