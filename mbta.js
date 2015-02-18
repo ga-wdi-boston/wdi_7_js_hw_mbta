d4bfce03a05ec646ba9f5ecf033af1e3dcf4e952
idelairre <idelairre@gmail.com>
2015-02-17 20:50:15 -0500

    corrected some typos added alert.

--- mbta.js
+++ mbta.js
@@ -1,12 +1,12 @@
 var MBTA = {
 
-lines = {
+lines: {
   "green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
   "red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
   "orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
   },
 
-  userInputs = function(){
+  userInputs: function(){
     this.startingLine = prompt("Enter the Starting Line: ");
     this.endingLine = prompt("Enter the Ending Line");
     this.startingStation = prompt("Enter the Starting Station");
@@ -16,7 +16,7 @@ lines = {
   // distance = mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)
   // distance = distance.abs
 
-  distanceToParkSt = function(line){
+  distanceToParkSt: function(line){
       var currentLine = this.lines[line],
       parkSt = currentLine.indexOf("Park Street");
       return function(endingStation){
@@ -25,7 +25,7 @@ lines = {
       }
   },
 
-  distanceToStation = function(){
+  distanceToStation: function(){
     this.startParkSt = this.distanceToParkSt(this.startingLine);
     this.endParkSt = this.distanceToParkSt(this.endingLine);
     this.firstStop = this.startParkSt(this.startingStation);
@@ -33,6 +33,10 @@ lines = {
     return this.firstStop + this.endingStop;
   }
 };
+
+MBTA.prompts();
+alert("The total number of stops for your travel is " + MBTA.distanceToStation() + " stops.");
+
 
 // alert("Staring at " + startingLine + " : " + startingStation);
 // alert("Ending at " + endingLine + " : " + endingStation);
