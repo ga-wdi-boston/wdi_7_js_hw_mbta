7345ce69dd4bfa7e59619ec3d38c2f6ba2854af6
LPayyapilli <luke.payyapilli@gmail.com>
2015-04-30 17:04:37 -0400

    First Pass

--- mbta.js
+++ mbta.js
@@ -1,21 +1,45 @@
 // Code here.
-var startingLine,
-startingStation,
-endingLine,
-endingStation;
+var startingLine = "Green",
+startingStation = "Copley",
+endingLine = "Orange",
+endingStation = "Forest Hills";
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+// startingLine = prompt("Enter the Starting Line: ");
+// startingStation = prompt("Enter the Starting Station");
+// endingLine = prompt("Enter the Ending Line");
+// endingStation =  prompt("Enter the Ending Station");
 
-alert("Staring at " + startingLine + " : " + startingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
+// alert("Staring at " + startingLine + " : " + startingStation);
+// alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+var lines = {
+  'Green':["Haymarket", "Government Center", "Park", "Boylston", "Arlington", "Copley"],
+  'Red': ["South Station", "Park", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"],
+  'Orange': ["North Station", "Haymarket", "Park", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
 };
+
+var startIndex = lines[startingLine].indexOf(startingStation);
+var endIndex = lines[endingLine].indexOf(endingStation);
+var toPark = Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf("Park"));
+var fromPark = Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf("Park"));
+
+var sameLine = function(p1, p2) {return Math.abs(startIndex - endIndex);}
+
+
+
+var diffLine = function(p1, p2) {return Math.abs(toPark + fromPark);}
+
+function getTrain (p1, p2) {
+  if (startingLine === endingLine) {
+    return sameLine();{
+
+    } else {
+      return diffLine();
+    }
+  }
+
+  console.log(getTrain());
+
+
 
