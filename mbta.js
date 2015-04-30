355eab775f6f0d3adb2dac8d72638e4e1996cd44
astoellis <astoellis@gmail.com>
2015-04-30 17:21:38 -0400

    yay I broke everything

--- mbta.js
+++ mbta.js
@@ -23,38 +23,35 @@ alert_str = 'Number of stops is: ';
 // Create a array for each line
 
 var myMBTA = {
+  lines : {
+    'green':['Haymarket', 'Government Center', 'Park St', 'Boylston', 'Arlington', 'Copley'],
+    'red': ['South Station', 'Park St', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+    'orange': ['North Station', 'Haymarket', 'Park St', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
+  },
 
+  abs : function(indexVal){
+    return Math.abs(indexVal);
+  },
+
+  getIndex : function(value, mbta_line){
+    return lines[mbta_line].indexOf(value);
+  },
+
+  calcSegment : function(stop_one, stop_two, line) {
+    return abs(getIndex(stop_one, line) - getIndex(stop_two, line));
+  },
+
+  diffLineDistance : function(firstStop, lastStop, firstLine, lastLine){
+    //Get distance to park st for both lines
+    firstSegment = this.calcSegment('Park St', firstStop, firstLine);
+    secondSegment = this.calcSegment('Park St', lastStop, lastLine);
+    return firstSegment + secondSegment;
+  }
 };
 
-lines = {
-  'green':['Haymarket', 'Government Center', 'Park St', 'Boylston', 'Arlington', 'Copley'],
-  'red': ['South Station', 'Park St', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
-  'orange': ['North Station', 'Haymarket', 'Park St', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
-};
 
-//Create a function that calculates the difference between
-sameLineDistance = function(startStop, endStop, startLine, endLine){
-  return Math.abs(lines[startLine].indexOf(startStop)-lines[endLine].indexOf(endStop));
-};
+if (startingLine === endingLine){
+  console.log(alert_str + myMBTA.calcSegment(startingStation,endingStation,startingLine));
+} else {console.log(alert_str + myMBTA.diffLineDistance(startingStation,endingStation, startingLine, endingLine));
 
-abs = function(indexVal){
-  return Math.abs(indexVal);
 }
-getIndex = function(value, mbta_line){
-  return lines[mbta_line].indexOf(value);
-}
-
-calcSegment = function(stop_one, stop_two, line) {
-  return abs(getIndex(stop_one, line) - getIndex(stop_two, line));
-};
-
-diffLineDistance = function(firstStop, lastStop, firstLine, lastLine){
-  //Get distance to park st for both lines
-  firstSegment = calcSegment('Park St', firstStop, firstLine);
-  secondSegment = calcSegment('Park St', lastStop, lastLine);
-  return firstSegment + secondSegment;
-};
-
-console.log(startingLine === endingLine ? (alert_str + sameLineDistance(startingStation, endingStation, startingLine, endingLine)): (alert_str + diffLineDistance(startingStation,endingStation, startingLine, endingLine)));
-
-
