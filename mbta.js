2dccc025dbecc257fe88ae7abf96d524bce559aa
Kinjalc <kinjal.chauhan@gmail.com>
2015-05-01 09:05:05 -0400

    git commit

--- mbta.js
+++ mbta.js
@@ -13,9 +13,18 @@ alert("Staring at " + startingLine + " : " + startingStation);
 alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
-lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+lvar lines = {
+  'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
+  'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
 };
 
+
+
+
+function trip(startingLine,startingStation,endingLine,endingStation){
+    if (startingLine === endingLine){
+       return  Math.abs(lines[startingLine].indexOf(startingStation) - lines[endingLine].indexOf(endingStation) );
+    }
+}
+console.log(trip("red","southstation","red","central"));
