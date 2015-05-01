d6abdd4c024d5c67badc0ce60bb6acb65497fdf6
Peter Masucci <peter.a.masucci@gmail.com>
2015-05-01 09:01:25 -0400

    Did a thing.

--- mbta.js
+++ mbta.js
@@ -4,18 +4,28 @@ startingStation,
 endingLine,
 endingStation;
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+// startingLine = prompt("Enter the Starting Line: ");
+// startingStation = prompt("Enter the Starting Station");
+// endingLine = prompt("Enter the Ending Line");
+// endingStation =  prompt("Enter the Ending Station");
 
-alert("Staring at " + startingLine + " : " + startingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
+// alert("Staring at " + startingLine + " : " + startingStation);
+// alert("Ending at " + endingLine + " : " + endingStation);
 
 // Create a array for each line
 lines = {
-  'green':[],
-  'red': [],
-  'orange': []
+  'green':['Haymarket', 'Government Center', 'Park', 'Boylston', 'Arlington', 'Copley'],
+  'red': ['South', 'Park', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+  'orange': ['North', 'Park', 'Kendall', 'State', 'Downtown', 'Chinatown', 'Back Bay', 'Forest Hills']
 };
+
+function planTrip(startingLine, startingStation, endingLine, endingStation){
+  if(startingLine === endingLine)
+  {
+    return Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf(endingStation));
+  }
+  else{
+    return Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf('Park')) + Math.abs(lines[endingLine].indexOf('Park') - lines[endingLine].indexOf(endingStation));
+  }
+}
 
